ecf696fc3909aabaa2fb6915d51d3068ad7bbd34
Anders Olson <sredna43@gmail.com>
2022-11-15 22:35:01 -0600

    Messing aroung with debugging

--- godot/Main.cs
+++ godot/Main.cs
@@ -3,21 +3,19 @@ using System;
 
 public class Main : Node2D
 {
+    Sprite test;
     public override void _Ready()
     {
-        Sprite mySprite = GetNode<Sprite>("Test");
-        bool test = mySprite.Visible;
-        GD.Print(test);
-        mySprite.Visible = false;
-
-        GD.Print(mySprite.Visible);
+        test = GetNode<Sprite>("Test");
     }
 
     public override void _Process(float delta)
     {
+        SceneTree tree = GetTree();
         if (delta > 0.0167)
         {
             GD.Print(delta);
+            GD.Print(test.Visible);
         }
     }
 }
