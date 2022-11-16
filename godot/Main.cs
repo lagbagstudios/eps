7be989050d8be1fda26d69d916d140a3cd3e4569
Anders Olson <sredna43@gmail.com>
2022-11-15 21:58:52 -0600

    add .editorconfig

--- godot/Main.cs
+++ godot/Main.cs
@@ -3,23 +3,21 @@ using System;
 
 public class Main : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         Sprite mySprite = GetNode<Sprite>("Test");
         bool test = mySprite.Visible;
         GD.Print(test);
         mySprite.Visible = false;
+
         GD.Print(mySprite.Visible);
     }
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //      
-    //  }
+    public override void _Process(float delta)
+    {
+        if (delta > 0.0167)
+        {
+            GD.Print(delta);
+        }
+    }
 }
