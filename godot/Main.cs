bc89ead212034938c841a50a70455756093ebed4
Anders Olson <sredna43@gmail.com>
2022-11-15 18:01:27 -0600

    Add C#

---
+++ godot/Main.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class Main : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Sprite mySprite = GetNode<Sprite>("Test");
+        bool test = mySprite.Visible;
+        GD.Print(test);
+        mySprite.Visible = false;
+        GD.Print(mySprite.Visible);
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
